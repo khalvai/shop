f26ac52b27dd33e60bb5adae3d8ec04235e5e0c6
khalvai <khalvayozbek@gmail.com>
2022-08-11 02:49:47 -0400

    Add login-register user contoller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param } from '@nestjs/common';
+import { Post, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Post('login-register')
+  async siginIn() {
+    return await `user sigin in`;
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id) {
+    return await `user get by id`;
+  }
+}
