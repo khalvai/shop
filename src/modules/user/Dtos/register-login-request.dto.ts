d0f4ac0da3ff5b2a0012ae736fe7f01f5d43d0ab
khalvai <khalvayozbek@gmail.com>
2022-08-12 02:14:33 -0400

    Add user login by password and phone number dto

--- src/modules/user/Dtos/register-login-request.dto.ts
+++ src/modules/user/Dtos/register-login-request.dto.ts
@@ -1,18 +1,27 @@
 import {
-    IsEmail,
+  IsEmail,
   IsNotEmpty,
   IsNumber,
+  IsPhoneNumber,
   IsString,
   MaxLength,
   MinLength,
 } from 'class-validator';
 
-export class LoginByEmialRequestDto {
+export class LoginByEmailRequestDto {
   @IsString()
-  @IsNotEmpty({message:"should have value"})
+  @IsNotEmpty({ message: 'should have value' })
   @IsEmail()
   email: string;
-
-
 }
 
+export class PhoneNumberAndPasswordDto {
+  @IsPhoneNumber('IR', { message: 'phone number should be correct' })
+  phoneNo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30, { message: "password shouldn't be longger than 30 letters." })
+  @MinLength(8, { message: "Password shouldn't be less than 8 letter" })
+  password: string;
+}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,29 +6,34 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Post, Get } from '@nestjs/common';
-import { LoginByEmialRequestDto } from './Dtos/register-login-request.dto';
+import {
+  LoginByEmailRequestDto,
+  PhoneNumberAndPasswordDto,
+} from './Dtos/register-login-request.dto';
 
 @Controller('users')
 export class UserController {
-
-  
   @Post('loginByEmail')
   async loginByEmail() {}
 
   @UsePipes(ValidationPipe)
   @Post('loginByPhoneNumber')
-  async lgoinByPhone() {}
+  async lgoinByPhone(@Body() loginByPhone) {
+    return await `it is working`;
+  }
 
   @UsePipes(ValidationPipe)
   @Post('/loginByemailAndPassword')
   @UsePipes(ValidationPipe)
   async loginByEmailAndPassword(
-    @Body() loginRegisterDto: LoginByEmialRequestDto,
+    @Body() loginRegisterDto: LoginByEmailRequestDto,
   ) {
     return await loginRegisterDto;
   }
 
   @UsePipes(ValidationPipe)
   @Post('loginByPhoneAndPassword')
-  async loginByPhone() {}
+  async loginByPhone(@Body() phoneNoAndPasswordDto: PhoneNumberAndPasswordDto) {
+    return ``;
+  }
 }
