b3dcf847635c82c34e5269f130dc87682f72fb3f
khalvai <khalvayozbek@gmail.com>
2022-08-23 10:24:09 -0400

    Create Error handling

---
+++ src/errorHandling/http-exception.filter.ts
@@ -0,0 +1,23 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const error = exception.getResponse();
+
+    response.status(status).json({
+      timestamp: new Date().toISOString(),
+      error,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './errorHandling/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(5000);
 }
